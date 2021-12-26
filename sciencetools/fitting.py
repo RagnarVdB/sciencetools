aeca98f2fd3e9df2c41774d77f02ddb34c6e06e9
Ragnar Van den Broeck <ragnar.van.den.broeck@gmail.com>
2021-12-26 13:19:40 +0100

    extra docs en bugfixes

--- sciencetools/fitting.py
+++ sciencetools/fitting.py
@@ -3,7 +3,7 @@ import scipy.optimize as opt
 from scipy.misc import derivative
 from scipy import stats
 from sciencetools.stats import chi2
-from sciencetools.misc import *
+from sciencetools.misc import substitute
 from inspect import getfullargspec
 import pandas as pd
 
@@ -33,7 +33,7 @@ def _fitParameters(x, y, dy, model, guess, bounds=None, method=None, respect_bou
         minobj = opt.minimize(f, guess, bounds=bounds, jac=jac, **kwargs)
     else:
         minobj = opt.minimize(f, guess, bounds=bounds, **kwargs)
-    return(minobj["x"])
+    return(minobj)
 
 def _errorFit2(x, y, dy, model, minimum):
     """Geeft onzekerheidsinterval rond fitparameters via tweede afgeleide"""
@@ -48,7 +48,6 @@ def _errorFit(x, y, dy, model, minimum, symmetric=False):
     uncertainties = []
     for i in range(len(minimum)):
         chi2_parameter = lambda p: chi2(x, y, dy, model, substitute(p, i, minimum))
-        chi2_vector = np.vectorize(chi2_parameter)
         minval = (minimum[i], chi2_parameter(minimum[i]))
         f = lambda p: chi2_parameter(p) - minval[1] - 1
         left = opt.fsolve(f, minval[0] *0.9997)[0]
@@ -61,7 +60,39 @@ def _errorFit(x, y, dy, model, minimum, symmetric=False):
     return np.array(uncertainties)
 
 def fit(x, y, dy, model, guess=None, bounds=None, method=None, error_method=1, respect_bounds=False, silent=False, pandas=True, **kwargs):
-    """Voert fit uit voor gegeven dataset en model"""
+    """
+    Voert fit uit voor gegeven dataset en model
+    
+    Parameters
+    ----------
+    x : array_like
+        x-waarden van datapunten
+    y : array_like
+        y-waarden van datapunten
+    dy : array_like
+        onzekerheid op y-waarden
+    model : callable
+        te fitten model. Eerste argument is x-waarde, andere de parameters
+    guess : list
+        startwaarden voor fitalgortime, lengte moet gelijk zijn aan aantal parameters
+    bounds : list
+        grenzen van de parameters, tuple van min en max voor elke parameter
+    method : str
+        algoritme gebruikt door scipy.optimize.minimize
+    error_method : 1 | 2
+        methode om fouten te bepalen
+        1 : chikwadraat + 1
+        2 : tweede afgeleide
+    respect_bounds : bool
+        True: gebruikt speciale gradient functie die model niet oproept met parameters buiten bounds
+    silent : bool
+        True: print geen p-waarden en andere
+    pandas : bool
+        True: geeft parameters en errors als pandas dataframe
+        False: geeft parameters en errors als numpy matrix
+    **kwargs
+        worden doorgegven aan scipy.optimize.minimize
+    """
     if type(dy) == int and dy == 0:
         dy = 1
         silent=True
@@ -70,7 +101,10 @@ def fit(x, y, dy, model, guess=None, bounds=None, method=None, error_method=1, r
     if guess is None:
         guess = [1]*(len(modelParams) - 1)
 
-    params = _fitParameters(x, y, dy, model, guess, bounds=bounds, method=method, respect_bounds=respect_bounds, **kwargs)
+    if len(modelParams) < len(guess):
+        modelParams = list(map(chr, range(97, 123)))[:len(guess)] # alfabet
+
+    params = _fitParameters(x, y, dy, model, guess, bounds=bounds, method=method, respect_bounds=respect_bounds, **kwargs)["x"]
     if error_method == 1:
         errors = _errorFit(x, y, dy, model, params, symmetric=True)
     else:
@@ -80,7 +114,7 @@ def fit(x, y, dy, model, guess=None, bounds=None, method=None, error_method=1, r
         ls, rls, p = goodness_fit(x, y, dy, model, params)
         print("Least squares value: " + str(round(ls, 3)))
         print("Reduced Least squares: " + str(round(rls, 3)))
-        print("p-value: " + str(round(p, 3)))
+        print("p-value: " + str(round(p, 5)))
     
     arr = np.array([params, np.abs(errors)]).T
     if not pandas or not pandasGlobal:
@@ -91,6 +125,9 @@ def fit(x, y, dy, model, guess=None, bounds=None, method=None, error_method=1, r
 def goodness_fit(x, y, dy, model, params):
     """Geeft chi2, reduced chi2 en p-waarde terug"""
     ls = chi2(x, y, dy, model, params)
-    df = len(x) - len(params)
+    if len(y.shape) == 1:
+        df = len(y) - len(params)
+    else:
+        df = np.prod(y.shape) - len(params)
     p = 1 - stats.chi2.cdf(ls, df)
     return ls, ls/df, p
